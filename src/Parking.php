b9722a6250d79568211462e69c8990b74c7d4535
vaxlrus <vaxl.rus@gmail.com>
2021-12-22 17:57:38 +0300

    #8 Добавлено исключение в конструктор парковки

--- src/Parking.php
+++ src/Parking.php
@@ -8,6 +8,28 @@ class Parking
 
     public function __construct(int $capacity)
     {
+        try
+        {
+            $this->parkingCreation($capacity);
+        }
+        catch (\DomainException $error)
+        {
+            echo $error->getMessage() . PHP_EOL;
+        }
+    }
+
+    private function parkingCreation(int $capacity)
+    {
+        if ($capacity == 0)
+        {
+            throw new \DomainException('Парковка не может быть создана без мест');
+        }
+
+        if ($capacity < 0)
+        {
+            throw new \DomainException('Парковка не может иметь отрицательную вместимость');
+        }
+
         $this->capacity = $capacity;
     }
 }
