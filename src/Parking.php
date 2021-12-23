fb7cba72b63888716c7cf0b70943cb300acca891
vaxlrus <vaxl.rus@gmail.com>
2021-12-23 22:18:15 +0300

    #10 Обновил название переменной

--- src/Parking.php
+++ src/Parking.php
@@ -4,32 +4,32 @@ namespace App;
 
 class Parking
 {
-    private int $parkingCapacity; // Общая вместимость паркинга
+    private int $totalCapacity; // Общая вместимость паркинга
     private array $carStorage = []; // Хранилище автомобилей
 
-    public function __construct(int $parkingCapacity)
+    public function __construct(int $totalCapacity)
     {
         // Если парковка создается с нулевым пространством
-        if ($parkingCapacity == 0)
+        if ($totalCapacity == 0)
         {
             throw new \DomainException('Парковка не может быть создана без мест');
         }
 
         // Если парковка создается с отрицательным пространством
-        if ($parkingCapacity < 0)
+        if ($totalCapacity < 0)
         {
             throw new \DomainException('Парковка не может иметь отрицательную вместимость');
         }
 
         // Задать вместимость парковки
-        $this->parkingCapacity = $parkingCapacity;
+        $this->$totalCapacity = $totalCapacity;
     }
 
     // Метод парковки авто
     public function parkAuto(Auto $auto): bool
     {
         // Если количество занятых мест равно максимальному на парковке, то она переполнена: return false
-        if (count($this->carStorage) == $this->parkingCapacity)
+        if (count($this->carStorage) == $this->totalCapacity)
         {
             return false;
         }
