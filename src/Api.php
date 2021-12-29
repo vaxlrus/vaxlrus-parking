e7202d22429bd2c6f3fd426f30188c685cd9709a
vaxlrus <vaxl.rus@gmail.com>
2021-12-29 20:24:29 +0300

    #20 Добавил дополнительные методы в API

--- src/Api.php
+++ src/Api.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Parking\Parking;
 use App\Repository;
+use App\Parking\Vehicle;
 
 class Api
 {
@@ -17,13 +18,84 @@ class Api
     // Создание парковки
     public function createParking(int $capacity): Parking
     {
-        echo $this->repo->nextId();
-
         // Создать парковку
         $parking = new Parking($capacity, $this->repo->nextId());
 
         // Сохранить парковку в файл
         $this->repo->save($parking);
+
+        return $parking;
+    }
+
+    // Получить все парковки
+    public function getAllParkings(): array
+    {
+        return $this->repo->loadAll();
+    }
+
+    // Получить конкретную парковку
+    public function getParking(int $id): Parking
+    {
+        return $this->repo->load($id);
+    }
+
+    // Запарковать авто
+    public function parkVehicle(int $parkingId, string $vehicleType, string $vin): Parking
+    {
+        // Загрузить объект парковки
+        $parking = $this->repo->load($parkingId);
+
+        // Если объект не является типом класса
+        $classType = "App\\Parking\\{$vehicleType}";
+
+        // Типы авто
+        $vehicleTypes = [
+            'Auto',
+            'Truck',
+            'Motorcycle',
+        ];
+
+        //        if (!class_exists($classType) && !is_subclass_of($classType, 'App\Vehicle'))
+        if (!in_array($vehicleType, $vehicleTypes))
+        {
+            throw new \DomainException('Объект типа ' . $vehicleType . ' не является транспортным средством');
+        }
+
+        // Создать тип транспортного средства
+        $vehicle = new $classType($vin);
+
+        // Припарковать авто
+        $parking->park($vehicle);
+
+        // Сохранить состояние
+        $this->repo->save($parking);
+
+        return $parking;
+    }
+
+    // Отпарковать авто
+    public function unparkVehicle(int $parkingId, string $vin): Parking
+    {
+        // Загрузить парковку
+        $parking = $this->repo->load($parkingId);
+
+        // Отпарковать авто
+        $parking->unpark($vin);
+
+        // Сохранить состояние
+        $this->repo->save($parking);
+
+        return $parking;
+    }
+
+    // Удаление парковки
+    public function removeParking(int $parkingId): Parking
+    {
+        // Загрузить объект парковки
+        $parking = $this->repo->load($parkingId);
+
+        // Удалить эту парковку
+        $this->repo->removeParking($parkingId);
 
         return $parking;
     }
